3335e1e4c7ac538580e575bca36a010447fe2aad
NguyenVanBo <nguyenvanbo1898@gmaill.com>
2023-12-04 02:14:42 +0700

    [Add] Create Post Repository

--- src/main/java/com/vti/BlogApp/repository/PostRepository.java
+++ src/main/java/com/vti/BlogApp/repository/PostRepository.java
@@ -3,5 +3,8 @@ package com.vti.BlogApp.repository;
 import com.vti.BlogApp.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends JpaRepository<Post,Long> {
+public interface PostRepository extends JpaRepository<Post,Long>
+//Kế thừa Lớp của JPA đã có sẵn CRUD chỉ việc lấy để dùng <Entity,Kiểu dữ liệu khóa chính>
+{
+
 }
