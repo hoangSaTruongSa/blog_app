61cdfeb7c79ef38117b50e9741a1a76624bac714
NguyenVanBo <nguyenvanbo1898@gmaill.com>
2023-12-04 02:13:58 +0700

    [Add] Create Post Mapper

--- src/main/java/com/vti/BlogApp/mapper/PostMapper.java
+++ src/main/java/com/vti/BlogApp/mapper/PostMapper.java
@@ -1,2 +1,31 @@
-package com.vti.BlogApp.mapper;public class PostMapper {
+package com.vti.BlogApp.mapper;
+
+import com.vti.BlogApp.dto.PostDto;
+import com.vti.BlogApp.entity.Post;
+import com.vti.BlogApp.form.PostCreateForm;
+
+public class PostMapper {
+    public static Post map(PostCreateForm form)
+    {
+        var post = new Post();
+        post.setTitle(form.getTitle());
+        post.setDescription(form.getDescription());
+        post.setContent(form.getContent());
+
+        return post;
+    }
+
+    public static PostDto map(Post post)
+    {
+        var postDto = new PostDto();
+
+        postDto.setId(post.getId());
+        postDto.setTitle(post.getTitle());
+        postDto.setDescription(post.getDescription());
+        postDto.setContent(post.getContent());
+        postDto.setCreatedAt(post.getCreatedAt());
+        postDto.setUpdatedAt(post.getUpdatedAt());
+
+        return postDto;
+    }
 }
