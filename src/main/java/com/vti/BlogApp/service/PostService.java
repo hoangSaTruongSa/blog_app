bda6313f1258a4c18df9346c93d90d3610cdf544
NguyenVanBo <nguyenvanbo1898@gmaill.com>
2023-12-04 02:15:20 +0700

    [Add] Create Post Service

--- src/main/java/com/vti/BlogApp/service/PostService.java
+++ src/main/java/com/vti/BlogApp/service/PostService.java
@@ -1,2 +1,8 @@
-package com.vti.BlogApp.service;public interface PostService {
+package com.vti.BlogApp.service;
+
+import com.vti.BlogApp.dto.PostDto;
+import com.vti.BlogApp.form.PostCreateForm;
+
+public interface PostService {
+    PostDto createPost(PostCreateForm form);
 }
