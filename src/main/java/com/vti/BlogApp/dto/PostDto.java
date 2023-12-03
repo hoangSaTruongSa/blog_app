a9093e8710d22dda92f7ad0900216555befc1fb8
NguyenVanBo <nguyenvanbo1898@gmaill.com>
2023-12-04 02:12:35 +0700

    [Add] Create Post Dto

--- src/main/java/com/vti/BlogApp/dto/PostDto.java
+++ src/main/java/com/vti/BlogApp/dto/PostDto.java
@@ -1,2 +1,18 @@
-package com.vti.BlogApp.dto;public class PostDto {
+package com.vti.BlogApp.dto;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class PostDto
+//Class Hiển thị kết quả cho người dùng
+{
+    private Long id;
+    private String title;
+    private String description;
+    private String content;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
