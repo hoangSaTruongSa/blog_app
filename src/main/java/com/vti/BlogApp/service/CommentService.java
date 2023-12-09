b99a8756f392415bd8fe1c7fddedbc536b5b5e03
NguyenVanBo <nguyenvanbo1898@gmaill.com>
2023-12-10 01:04:22 +0700

    [Add]Create Comment Service

---
+++ src/main/java/com/vti/BlogApp/service/CommentService.java
@@ -0,0 +1,5 @@
+package com.vti.BlogApp.service;
+
+public interface CommentService {
+
+}
---
+++ src/main/java/com/vti/BlogApp/service/CommentServiceImpl.java
@@ -0,0 +1,4 @@
+package com.vti.BlogApp.service;
+
+public class CommentServiceImpl implements CommentService{
+}
