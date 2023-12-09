a1e6614e9589d795e3c91595dc2c8819866206c3
NguyenVanBo <nguyenvanbo1898@gmaill.com>
2023-12-10 01:34:59 +0700

    [Add]Create Comment Mapper

---
+++ src/main/java/com/vti/BlogApp/mapper/CommentMapper.java
@@ -0,0 +1,29 @@
+package com.vti.BlogApp.mapper;
+
+import com.vti.BlogApp.dto.CommentDto;
+import com.vti.BlogApp.entity.Comment;
+import com.vti.BlogApp.form.CommentCreateForm;
+
+public class CommentMapper {
+    public static Comment map(CommentCreateForm form)
+    {
+        var comment = new Comment();
+
+        comment.setName(form.getName());
+        comment.setEmail(form.getEmail());
+        comment.setBody(form.getBody());
+        return comment;
+    }
+
+    public static CommentDto map(Comment comment)
+    {
+        var commentDto = new CommentDto();
+
+        commentDto.setId(comment.getId());
+        commentDto.setName(comment.getName());
+        commentDto.setBody(comment.getBody());
+        commentDto.setCreatedAt(comment.getCreatedAt());
+        commentDto.setUpdatedAt(comment.getUpdatedAt());
+        return commentDto;
+    }
+}
