3a1ffad78315fe737ce906892843606e53ed063e
NguyenVanBo <nguyenvanbo1898@gmaill.com>
2023-12-10 01:17:24 +0700

    [Add]Create Comment Dto

---
+++ src/main/java/com/vti/BlogApp/dto/CommentDto.java
@@ -0,0 +1,18 @@
+package com.vti.BlogApp.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class CommentDto {
+    private Long id;
+    private String name;
+    private String email;
+    private String body;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
