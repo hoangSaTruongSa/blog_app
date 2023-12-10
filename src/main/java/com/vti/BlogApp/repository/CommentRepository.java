7d8f26f0f619adaf6bfaf9b09c0b5ef5d6960139
NguyenVanBo <nguyenvanbo1898@gmaill.com>
2023-12-11 03:18:48 +0700

    [Add] HQL Anotation Query In Repository

--- src/main/java/com/vti/BlogApp/repository/CommentRepository.java
+++ src/main/java/com/vti/BlogApp/repository/CommentRepository.java
@@ -4,7 +4,16 @@ import com.vti.BlogApp.entity.Comment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    Page<Comment> findCommentByPostId(Long postId, Pageable pageable);
+   /* //Method Name
+    Page<Comment> findCommentByPostId(Long postId, Pageable pageable);*/
+
+    //@Query annotation
+    @Query("FROM Comment WHERE post.id =:postId")
+    Page<Comment> findCommentByPostId(@Param("postId") Long postId, Pageable pageable);
+
+
 }
