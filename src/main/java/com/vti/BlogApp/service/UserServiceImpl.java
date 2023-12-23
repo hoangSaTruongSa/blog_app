1334a0e1b44e0af4b53e3b758c7a3eac10c17a31
NguyenVanBo <nguyenvanbo1898@gmaill.com>
2023-12-23 21:32:49 +0700

    Password Encode

--- src/main/java/com/vti/BlogApp/service/UserServiceImpl.java
+++ src/main/java/com/vti/BlogApp/service/UserServiceImpl.java
@@ -5,15 +5,19 @@ import com.vti.BlogApp.form.UserCreateForm;
 import com.vti.BlogApp.mapper.UserMapper;
 import com.vti.BlogApp.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @AllArgsConstructor
 @Service
 public class UserServiceImpl implements UserService{
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
     @Override
     public UserDto createUser(UserCreateForm form) {
         var user = UserMapper.map(form);
+        var encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
         var userSaved = userRepository.save(user);
         return UserMapper.map(userSaved);
     }
